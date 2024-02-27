b770b54334a37a8a14b984f3675f9502dd2c68da
b4mbus <danielzaradny@gmail.com>
2024-02-27 19:21:15 +0100

    asdasdasd

--- flake.nix
+++ flake.nix
@@ -6,24 +6,34 @@
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = inputs @ { ...}:
+  outputs = inputs @ {...}:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       systems = import inputs.systems;
       perSystem = {
         pkgs,
         inputs',
         system,
-        lib,
-        self',
         ...
       }: let
-        buildDeps = [ pkgs.fmt ];
-        nativeDeps = [ pkgs.cmake pkgs.pkg-config ];
-
-        rootDir = ./.;
+        buildDeps = [pkgs.fmt];
+        nativeDeps = [pkgs.cmake pkgs.pkg-config];
       in {
-        devShells = import ./nix/shell.nix {
-          inherit pkgs buildDeps nativeDeps;
+        formatter = pkgs.alejandra;
+        devShells = let
+          baseDevShellAttrs = {
+            hardeningDisable = ["all"];
+
+            packages =
+              nativeDeps
+              ++ [
+                pkgs.act
+                pkgs.just
+              ];
+
+            buildInputs = buildDeps;
+          };
+        in {
+          devGcc = pkgs.mkShell.override {stdenv = pkgs.gcc12.stdenv;} baseDevShellAttrs;
         };
       };
     };
