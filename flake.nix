67202bd18cbcbdf2aaabb5e16e7fa25982f3199a
Dich0tomy <{ID}+{username}@users.noreply.github.com>
2026-07-09 21:22:33 +0200

    asdsa

--- flake.nix
+++ flake.nix
@@ -8,11 +8,38 @@
 
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} (
-      { flake-parts-lib, ... }: {
+      {flake-parts-lib, ...}: {
         systems = ["x86_64-linux" "aarch64-linux"];
-        perSystem = {pkgs, inputs', ...}: let
+        perSystem = {
+          pkgs,
+          inputs',
+          ...
+        }: let
         in {
           formatter = pkgs.alejandra;
+
+          packages.default = pkgs.stdenv.mkDerivation {
+            pname = "repro";
+            version = "0.1.0";
+
+            src = ./.;
+
+            nativeBuildInputs = [
+            	pkgs.cmake pkgs.meson pkgs.pkg-config pkgs.ninja
+            ];
+
+            buildInputs = [
+            pkgs.fmt
+            	inputs'.dire.packages.dev
+            ];
+
+            configurePhase = ''
+              meson setup build
+            '';
+            buildPhase = ''
+              meson compile -C build
+            '';
+          };
 
           devShells.default = pkgs.mkShell.override {stdenv = pkgs.llvmPackages_17.stdenv;} {
             hardeningDisable = ["all"];
@@ -23,7 +50,7 @@
               pkgs.ninja
               pkgs.pkg-config
 
-							pkgs.fmt
+              pkgs.fmt
 
               inputs'.dire.packages.dev
             ];
