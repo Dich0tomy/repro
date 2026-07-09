313a85700b31e0367b136d603da457cf0a7056b9
Dich0tomy <{ID}+{username}@users.noreply.github.com>
2026-07-09 21:22:33 +0200

    asda

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
-#include <cpptrace/cpptrace.hpp>
+#include <fmt/format.h>
+#include <dire/all.hpp>
 
-int main() { cpptrace::generate_trace().print(); }
+int main() {
+  fmt::print(
+  	"Some other: {}\nConfig path: {}",
+		dire::user::picture_dir()->string(),
+		dire::project::config_local_dir(
+			dire::project::name("me", "dich0tomy", "blanket")
+		)->string()
+	);
+}
