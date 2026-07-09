240a223ea6f2961373e331ca518b75716e40c965
Dich0tomy <{ID}+{username}@users.noreply.github.com>
2026-07-09 21:22:33 +0200

    asd

--- flake.nix
+++ flake.nix
@@ -1,15 +1,14 @@
 {
-  description = "A very basic flake";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { nixpkgs }: {
+		formatter = pkgs.x86_64-linux.nixfmt-tree;
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+		devShells.x86_64-linux.default = pkgs.mkShell.override {stdenv = pkgs.useMoldLinker pkgs.gcc14Stdenv;} {
+			hardeningDisable = ["all"];
+			nativeBuildInputs = [pkgs.cmake];
+		};
+	}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,55 @@
-#include <memory>
+#include <fmt/format.h>
 
-#include <drogon/drogon.h>
+#include <portaudio.h>
 
-#include <controller.hpp>
+auto init_portaudio() -> bool {
+  auto const pa_init_result = Pa_Initialize();
+  auto const pa_init_error_text = Pa_GetErrorText(pa_init_result);
+  auto const pa_init_successful = (pa_init_result == paNoError);
+
+  fmt::println("PortAudio initialization result: {} {}", pa_init_result, pa_init_error_text);
+
+  if (not pa_init_successful) {
+    fmt::println(
+			stderr,
+			"Failed to initialize portaudio. Input and audio playback "
+			"won't be available.\n"
+			"Error: {}",
+			pa_init_error_text
+		);
+    return false;
+  }
+
+  auto num_devices = Pa_GetDeviceCount();
+  if (num_devices < 0) {
+    fmt::println(stderr, "PortAudio returned an error: {}.", Pa_GetErrorText(pa_init_result));
+    return false;
+  } else if (num_devices == 0) {
+    fmt::println(stderr, "PortAudio hadn't found any feasible devices. The " "repl audio feature will be disabled.");
+    return false;
+  }
+
+  for (auto i = 0; i < num_devices; i++) {
+    auto const device_info = Pa_GetDeviceInfo(i);
+    fmt::println("Found audio device: [{}] {}", i, device_info->name);
+  }
+
+  return true;
+}
 
 auto main() -> int {
-	drogon::app()
-			.registerController(std::make_shared<DebugPathController>())
-			.addListener("127.0.0.1", 1234)
-			.run();
+  init_portaudio();
+
+  PaStream *stream = nullptr;
+  Pa_Initialize();
+  Pa_OpenDefaultStream(
+		&stream,
+		0, // either 1 0 / 0 1
+		1,
+		paInt16, // sample format
+		24000,
+		960,
+		nullptr,
+		nullptr
+	);
 }
